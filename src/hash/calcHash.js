c565d4d6800c9453c4e52573952e67a9a1854fcb
Andronio <gprsboost03@mail.ru>
2024-05-01 22:34:04 +0500

    hash, readStream

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const { createHash } = await import('node:crypto');
+
+const algo = 'sha256';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash(algo);
+    hash.setEncoding('hex');
+    const input = createReadStream(join(__dirname, 'files', 'fileToCalculateHashFor.txt'), 'utf-8');
+    return pipeline(input, hash, process.stdout);
 };
 
 await calculateHash();
--- src/streams/read_pipe.js
+++ src/streams/read_pipe.js
@@ -1,4 +1,5 @@
 import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
 
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
@@ -8,17 +9,7 @@ const __dirname = dirname(__filename);
 
 const read = async () => {
     const readStream = createReadStream(join(__dirname, 'files', 'fileToRead.txt'), 'utf-8');
-    return new Promise((resolve, reject) => {
-        readStream.on('data', (chunk) => {
-            process.stdout.write(chunk);
-        });
-        readStream.on('end', () => {
-            resolve();
-        });
-        readStream.on('error', (err) => {
-            reject(err);
-        });
-    });
+    return pipeline(readStream, process.stdout);
 };
 
 await read();
