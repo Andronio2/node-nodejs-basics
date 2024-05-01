227301fb1c9fb43ad4abde492fddfae686c0532f
Andronio <gprsboost03@mail.ru>
2024-05-01 22:41:51 +0500

    writeStream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import { createWriteStream } from 'node:fs';
+
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILENAME = 'fileToWrite.txt';
 const write = async () => {
-    // Write your code here 
+    const fstream = createWriteStream(join(__dirname, 'files', FILENAME));
+    process.stdin.on('data', (chunk) => {
+        fstream.write(chunk);
+        const str = chunk.toString().toLowerCase();
+        if (str.includes('close')) process.exit(0);
+    });
+    process.on('exit', () => {
+        fstream.end();
+    });
 };
 
 await write();
