12f2eae15542d52682ffbacbb0f9ffb8cc7e7265
Andronio <gprsboost03@mail.ru>
2024-05-04 23:17:55 +0500

    gzip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises'
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const IN_FILENAME = 'fileToCompress.txt';
+const OUT_FILENAME = 'archive.gz';
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(join(__dirname, 'files', IN_FILENAME), 'utf-8');
+    const writeStream = createWriteStream(join(__dirname, 'files', OUT_FILENAME));
+    const compressStream = createGzip();
+    return pipeline(readStream, compressStream, writeStream);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+import { unzip, createUnzip } from 'zlib';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const IN_FILENAME = 'archive.gz';
+const OUT_FILENAME = 'fileToDecompress.txt';
+
 const decompress = async () => {
-    // Write your code here 
+    const readStream = createReadStream(join(__dirname, 'files', IN_FILENAME));
+    const writeStream = createWriteStream(join(__dirname, 'files', OUT_FILENAME));
+    const decompressStream = createUnzip();
+    await pipeline(readStream, decompressStream, writeStream);
 };
 
 await decompress();
