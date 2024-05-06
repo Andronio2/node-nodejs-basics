f1c63941ab109dcc6b7b52451b3fc833ca611aff
Andronio <gprsboost03@mail.ru>
2024-05-07 00:45:04 +0500

    child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,33 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// const spawnChildProcess = async (args) => {
+//     // Write your code here
+// };
+
+// // Put your arguments in function call to test this functionality
+// spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+import { fork } from 'node:child_process';
+
+const file = join(__dirname, 'files', 'script.js');
+const child = fork(file, process.argv.slice(2), { silent: true });
+
+child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+child.on('close', (code) => {
+    console.log(`детский процесс завершился с кодом ${code}`);
+});
+
+child.stdin.write('hello');
+setTimeout(() => {
+    child.stdin.write('CLOSE');
+}, 1000);
